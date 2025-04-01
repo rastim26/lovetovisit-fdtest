9f3340e2aa9fb835bc5001d18fca6d143bffb09f
Rastim Safarov <rastim.safarov@appius.com>
2025-04-01 13:58:25 +0100

    feat: Add On click function

--- src/Views/Task2/Task2.tsx
+++ src/Views/Task2/Task2.tsx
@@ -4,7 +4,7 @@ import Button from './Components/Button/button'
 const Task2 = () => {
     return (
         <div className="task2">
-            <Button className="btn btn--primary" text="Primary"/>
+            <Button className="btn btn--primary" text="Primary" onClick={() => console.log('Priamry button is clicked')}/>
             <Button className="btn btn--secondary" text="Secondary"/>
             <Button className="btn btn--tertiary" text="Tertiary"/>
             <Button className="btn btn--disabled" text="Disabled" disabled />
