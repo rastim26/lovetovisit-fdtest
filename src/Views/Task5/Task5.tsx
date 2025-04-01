e6c781e14736ae24ec84474935091bc845aca0f2
Rastim Safarov <rastim.safarov@appius.com>
2025-04-01 22:45:30 +0100

    feat: Complete Task 5

--- src/Views/Task5/Task5.tsx
+++ src/Views/Task5/Task5.tsx
@@ -2,12 +2,22 @@ import './task5.css'
 import { useAppSelector } from '../../store/store.ts'
 import { getUsers } from './selectors.ts'
 
-const Member = () => {
+interface MemberProps {
+    member: {
+        firstName: string
+        lastName: string
+        age: number
+        membership: string
+    }
+}
+
+const Member = ({member}: MemberProps) => {
+    const { firstName, lastName, age, membership } = member;
     return (
         <div className="member">
-            <span>Name:</span> <span>John Doe</span>
-            <span>Age:</span> <span>20</span>
-            <span>Membership:</span> <span>Gold</span>
+            <span>Name:</span> <span>{firstName + ' ' + lastName}</span>
+            <span>Age:</span> <span>{age}</span>
+            <span>Membership:</span> <span>{membership}</span>
         </div>
     )
 }
@@ -18,26 +28,34 @@ const Task5 = () => {
         <div className="task5">
             <section data-testid="gold-member">
                 <h2>Gold Members</h2>
-                {members.map((member) => (
-                    <Member />
+                {members.filter((member) => (
+                    member.membership === '001'
+                )).map((member, index) => (    
+                    <Member key={index} member={member} />
                 ))}
             </section>
             <section data-testid="silver-member">
                 <h2>Silver Members</h2>
-                {members.map((member) => (
-                    <Member />
+                {members.filter((member) => (
+                    member.membership === '002'
+                )).map((member, index) => (
+                    <Member key={index} member={member} />
                 ))}
             </section>
             <section data-testid="bronze-member">
                 <h2>Bronze Members</h2>
-                {members.map((member) => (
-                    <Member />
+                {members.filter((member) => (
+                    member.membership === '003'
+                )).map((member, index) => (
+                    <Member key={index} member={member} />
                 ))}
             </section>
             <section data-testid="non-member">
                 <h2>Non Members</h2>
-                {members.map((member) => (
-                    <Member />
+                {members.filter((member) => (
+                    member.membership === '004'
+                )).map((member, index) => (
+                    <Member key={index} member={member} />
                 ))}
             </section>
         </div>
